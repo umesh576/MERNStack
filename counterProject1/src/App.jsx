f612ce3689d9ef49ac9d572d279b8cea4eb25d10
Umesh Joshi <uj19343@gmail.com>
2025-06-18 19:28:14 +0545

    Small value counter project is added in repo and learn about the hook and state concept in the react

--- counterProject1/src/App.jsx
+++ counterProject1/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* let explain how useState work
+  
+  actually counter is a variable and setCounter is a funcction which is responsible for the cahnge the state of the counter 
+  and useState(2) means that the set the counter default value is 2
+  */
+  const [counter, setCounter] = useState(2);
+  // let counter = 5;
+  const addValue = () => {
+    /*  setcounter is a functon or method which can cahnge the value or satet of the counter and update in all the state */
 
+    if (counter >= 10) {
+      console.log("value Overflow");
+    } else {
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  };
+  const decValue = () => {
+    if (counter <= 0) {
+      console.log("Value underflow");
+    } else {
+      setCounter(counter - 1);
+      console.log(counter);
+    }
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>counter value</h1>
+      <p id="hey">counter value: {counter}</p>
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={decValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
