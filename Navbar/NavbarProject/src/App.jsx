28eb514c6d36cd8e90d78ac260bf67c5abf2457f
Umesh Joshi <uj19343@gmail.com>
2025-06-21 17:40:05 +0545

    new project staring

---
+++ Navbar/NavbarProject/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="w-screen h-full text-red flex">
+      <ul className="flex gap-5">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">ContactUs</a>
+        </li>
+        <li>
+          <a href="#">emergencyService</a>
+        </li>
+        <li>
+          <a href="#">AboutUs</a>
+        </li>
+        <li>
+          <a href="#">login/signup</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ beginnerReactProject02/project2/src/StudentCard.jsx
@@ -0,0 +1,6 @@
+const StudentCard = (props) => {
+  console.log(props);
+  return <div>{props.stName}</div>;
+};
+
+export default StudentCard;
