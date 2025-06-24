645b26c350691a8562b942bc33242e0a9b04540f
Umesh Joshi <uj19343@gmail.com>
2025-06-24 22:31:27 +0545

    new update in the ui

--- currecyConverterProject03/currencyConverter/src/App.jsx
+++ currecyConverterProject03/currencyConverter/src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+// import React, { useState } from "react";
 
 const App = () => {
+  // const [fromcurrency, setFromCurrency] = useState();
+  // const [toCurrency, setToCurrency] = useState();
+
   return (
     <div className="w-screen h-screen flex justify-center items-center ">
       <div className="p-4 px-6 bg-white backdrop-blur-md border border-white/30 rounded-xl shadow-lg">
+        <h1 className="text-center text-lg">currencyConverter</h1>
         <div>
           <div className="w-full flex justify-between bg-white border border-white/30 rounded-xl shadow-lg p-6 ">
             <div>
@@ -19,11 +23,11 @@ const App = () => {
             </div>
           </div>
           <div className="w-full flex justify-center">
-            <button className="px-3 py-1 bg-blue-500 text-white rounded-lg cursor-pointer">
+            <button className="px-3 z-10 py-1 bg-blue-500 text-white rounded-lg cursor-pointer">
               Swap
             </button>
           </div>
-          <div className="flex backdrop-blur-md bg-white/10 border border-white/30 rounded-xl shadow-lg p-6">
+          <div className="flex backdrop-blur-md bg-white/10 border border-white/30 rounded-xl shadow-lg p-6 ">
             <div>
               <p>To</p>
               <input type="number" className="bg-gray-200" />
@@ -31,8 +35,8 @@ const App = () => {
             <div>
               <p>Currecy Type</p>
               <select name="" id="currencyType">
-                <option value="USD">USD</option>
-                <option value="NPR">NPR</option>
+                <option value={"toCurrency"}>NPR</option>
+                <option value={"toCurrency"}>USD</option>
               </select>
             </div>
           </div>
